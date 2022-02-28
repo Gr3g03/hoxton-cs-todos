2199681907e5e75aa639aedfca42047735350160
Gr3g03 <71180086+Gr3g03@users.noreply.github.com>
2022-02-28 15:53:30 +0100

    working with test functions

---
+++ src/App.test.tsx
@@ -0,0 +1,7 @@
+import { describe, it, expect } from "vitest";
+import { fireEvent, render, screen } from "@testing-library/react";
+import App, { addTodo, removeTodo } from './App'
+
+
+
+
--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,54 @@
 import { useEffect, useState } from 'react'
 import './App.css'
 
-type Todos = {
+// type Todos = {
 
-  userId: number,
-  id: number,
-  title: string,
-  completed: boolean
+//   userId: number,
+//   id: number,
+//   title: string,
+//   completed: boolean
+// }
+
+
+export type Todos = {
+  id: number
+  title: string
 }
+
+export function addTodo(todos: Todos[], title: string) {
+  const newTodo = { id: todos.length + 1, title: title }
+  return [...todos, newTodo]
+}
+
+export function removeTodo(todos: Todos[], id: number) {
+  return todos.filter(todo => todo.id !== id)
+}
+
 
 function App() {
   const [todos, setTodos] = useState<Todos[]>([])
 
-  useEffect(() => {
-    fetch('https://jsonplaceholder.typicode.com/todos')
-      .then(resp => resp.json())
-      .then(todos => setTodos(todos))
-  }, [])
+  // useEffect(() => {
+  //   fetch('https://jsonplaceholder.typicode.com/todos')
+  //     .then(resp => resp.json())
+  //     .then(todos => setTodos(todos))
+  // }, [])
 
 
-  function removeTodo(id: number): void {
-    const updateTodo = todos.filter(todo => todo.id !== id)
-    setTodos(updateTodo)
-  }
+  // function removeTodo(id: number): void {
+  //   const updateTodo = todos.filter(todo => todo.id !== id)
+  //   setTodos(updateTodo)
+  // }
 
-  function addNewTodo(title: string) {
-    const newTodo: Todos = {
-      userId: 1,
-      id: todos.length + 1,
-      title: title,
-      completed: false
-    }
-    setTodos([newTodo, ...todos])
-  }
+  // function addNewTodo(title: string) {
+  //   const newTodo: Todos = {
+  //     userId: 1,
+  //     id: todos.length + 1,
+  //     title: title,
+  //     completed: false
+  //   }
+  //   setTodos([newTodo, ...todos])
+  // }
 
 
   return (
@@ -42,8 +58,9 @@ function App() {
 
       <form onSubmit={(e) => {
         e.preventDefault()
-        addNewTodo(e.target.text.value)
+        const updatedTodo = addTodo(todos, e.target.text.value)
         e.target.reset()
+        setTodos(updatedTodo)
       }}>
         <input type="text" name='text' placeholder='Add a quote' />
         <button>Add</button>
@@ -53,7 +70,10 @@ function App() {
         {
           todos.map(todo =>
             <li key={todo.id}>
-              {todo.title} <button onClick={() => { removeTodo(todo.id) }}>x</button>
+              {todo.title} <button onClick={() => {
+                const updatedTodo = removeTodo(todos, todo.id)
+                setTodos(updatedTodo)
+              }}>x</button>
             </li>
           )}
       </ul>
