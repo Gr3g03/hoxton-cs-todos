49221da1dc62326f9abaf018278d9701a5366702
Gr3g03 <71180086+Gr3g03@users.noreply.github.com>
2022-02-28 16:20:31 +0100

    add todo + removo todo test functions done

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,7 +1,22 @@
 import { describe, it, expect } from "vitest";
 import { fireEvent, render, screen } from "@testing-library/react";
-import App, { addTodo, removeTodo } from './App'
+import App, { addTodo, removeTodo, Todos } from './App'
 
 
+// type todos=  Todos
+
+describe('addTodo', () => {
+    it('Add a todo when preses ADD', () => {
+        const AddButton = addTodo([], 'todo')
+        expect(AddButton).toMatchObject([{ id: 1, title: 'todo' }])
+    })
+
+})
 
 
+describe('removeTodo', () => {
+    it('removes a todo when pressed X', () => {
+        const result = removeTodo([], 1)
+        expect(result).toMatchObject([])
+    })
+})
